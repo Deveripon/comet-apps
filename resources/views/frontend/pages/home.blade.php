b60171b7ceab68d6d63985e5122e00e6de7d82b7
devripon <you@example.com>
2022-09-21 12:44:14 +0600

    expertise Section Done

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -3,38 +3,38 @@
 @section('main')
 
 @php
-  $slider = App\Models\Slider::get()
+$slider = App\Models\Slider::get()
 @endphp
 
 <section id="home">
-  <!-- Home Slider-->
-  <div id="home-slider" class="flexslider">
- 
-    <ul class="slides">
-      @foreach($slider as $slide)
-      <li>
-        <img src="{{url('storage/slider_image/'.$slide -> photo)}}" alt="">
-        <div class="slide-wrap">
-          <div class="slide-content">
-            <div class="container">
-              <h1>{{$slide -> title}}</h1><span class="red-dot"></span></h1>
-              <h6>{{$slide -> subtile}}</h6>
+    <!-- Home Slider-->
+    <div id="home-slider" class="flexslider">
 
-              <p>
-                @foreach(json_decode($slide -> buttons) as $btn)
-                <a href="{{$btn -> btn_link}}" class="{{$btn -> btn_type}}">{{$btn -> btn_name}}</a>
-                @endforeach
-              </p>
-            </div>
-          </div>
-        </div>
-      </li>
-      @endforeach
-    </ul>
+        <ul class="slides">
+            @foreach($slider as $slide)
+            <li>
+                <img src="{{url('storage/slider_image/'.$slide -> photo)}}" alt="">
+                <div class="slide-wrap">
+                    <div class="slide-content">
+                        <div class="container">
+                            <h1>{{$slide -> title}}</h1><span class="red-dot"></span></h1>
+                            <h6>{{$slide -> subtile}}</h6>
+
+                            <p>
+                                @foreach(json_decode($slide -> buttons) as $btn)
+                                <a href="{{$btn -> btn_link}}" class="{{$btn -> btn_type}}">{{$btn -> btn_name}}</a>
+                                @endforeach
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            @endforeach
+        </ul>
 
 
-  </div>
-  <!-- End Home Slider-->
+    </div>
+    <!-- End Home Slider-->
 </section>
 
 @include('frontend.section.about')
--- resources/views/frontend/section/expertise.blade.php
+++ resources/views/frontend/section/expertise.blade.php
@@ -1,4 +1,3 @@
-
 @php
 
 $expertise = App\Models\Expertise::latest()->get()->where('status', '=', true ) -> take(4)
@@ -9,42 +8,40 @@ $expertise = App\Models\Expertise::latest()->get()->where('status', '=', true ) 
 
 <section class="p-0 b-0">
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-6 col-sm-4 img-side img-left mb-0">
-          <div class="img-holder">
-            <img src="frontend/images/bg/33.jpg" alt="" class="bg-img">
-            <div class="centrize">
-              <div class="v-center">
-                <div class="title txt-xs-center">
-                  <h4 class="upper">This is what we love to do.</h4>
-                  <h3>Expertise<span class="red-dot"></span></h3>
-                  <hr>
+        <div class="row">
+            <div class="col-md-6 col-sm-4 img-side img-left mb-0">
+                <div class="img-holder">
+                    <img src="frontend/images/bg/33.jpg" alt="" class="bg-img">
+                    <div class="centrize">
+                        <div class="v-center">
+                            <div class="title txt-xs-center">
+                                <h4 class="upper">This is what we love to do.</h4>
+                                <h3>Expertise<span class="red-dot"></span></h3>
+                                <hr>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-        <!-- end of side background image-->
-        <div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4">
-          <div class="services">
-            <div class="row">
-              
-            @foreach($expertise as $item)
-
-            <div class="col-sm-6 border-bottom border-right">
-                <div class="service"><i class="{{$item ->icon}}"></i>
-                  <h4>{{$item -> title}}</h4>
-                  <hr>
-                  <p class="alt-paragraph">{{$item ->desc}}</p>
-                </div>
-                <!-- end of service-->
-              </div>
-
-            @endforeach
+            <!-- end of side background image-->
+            <div class="col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-4">
+                <div class="services">
+                    <div class="row">
 
 
+                        @foreach($expertise as $item)
 
-              <!-- <div class="col-sm-6 border-bottom border-right">
+                        <div class="col-sm-6 {{$class}}">
+                            <div class="service"><i class="{{$item ->icon}}"></i>
+                                <h4>{{$item -> title}}</h4>
+                                <hr>
+                                aa <p class="alt-paragraph">{{$item -> desc}}</p>
+                            </div>
+                            <!-- end of service-->
+                        </div>
+
+                        @endforeach
+                        <!-- <div class="col-sm-6 border-bottom border-right">
                 <div class="service"><i class="icon-focus"></i><span class="back-icon"><i class="icon-focus"></i></span>
                   <h4>Branding</h4>
                   <hr>
@@ -53,7 +50,7 @@ $expertise = App\Models\Expertise::latest()->get()->where('status', '=', true ) 
 
               </div> -->
 
-              <!-- <div class="col-sm-6 border-bottom">
+                        <!-- <div class="col-sm-6 border-bottom">
                 <div class="service"><i class="icon-layers"></i><span class="back-icon"><i class="icon-layers"></i></span>
                   <h4>Interactive</h4>
                   <hr>
@@ -62,7 +59,7 @@ $expertise = App\Models\Expertise::latest()->get()->where('status', '=', true ) 
 
               </div> -->
 
-              <!-- <div class="col-sm-6 border-bottom border-right">
+                        <!-- <div class="col-sm-6 border-bottom border-right">
                 <div class="service"><i class="icon-mobile"></i><span class="back-icon"><i class="icon-mobile"></i></span>
                   <h4>Production</h4>
                   <hr>
@@ -71,7 +68,7 @@ $expertise = App\Models\Expertise::latest()->get()->where('status', '=', true ) 
 
               </div> -->
 
-             <!--  <div class="col-sm-6 border-bottom">
+                        <!--  <div class="col-sm-6 border-bottom">
                 <div class="service"><i class="icon-globe"></i><span class="back-icon"><i class="icon-globe"></i></span>
                   <h4>Editing</h4>
                   <hr>
@@ -81,11 +78,11 @@ $expertise = App\Models\Expertise::latest()->get()->where('status', '=', true ) 
               </div> -->
 
 
+                    </div>
+                </div>
+                <!-- end of row-->
             </div>
-          </div>
-          <!-- end of row-->
         </div>
-      </div>
-      <!-- end of row -->
+        <!-- end of row -->
     </div>
-  </section>
+</section>
